4ce3e0787c0dbd36c2093286e8bc5c15b0508768
kennyster92 <kevinceni@gmail.com>
2019-04-01 13:23:36 +0200

    Structure improved

---
+++ bin/test/Testing.py
@@ -0,0 +1,16 @@
+class Tester:
+
+    def __init__(self, model):
+        """Initializes a new instance of the Tester class.
+        Args:
+            model: The model to use for testing.
+        """
+
+        self.model = model
+
+    def predict(self):
+        """Predicts subjects based on a pre-trained model.
+        This is the main method, which will call the other methods.
+        """
+
+        return None
---
+++ bin/train/Training.py
@@ -0,0 +1,17 @@
+class Trainer:
+
+    def __init__(self):
+
+        self.epochs = 20
+
+
+    def train(self):
+        """Trains a model, i.e. runs the epochs loop.
+        This is the main method, which will call the other methods.
+        """
+
+        return None
+
+    def validate(self):
+
+        return None

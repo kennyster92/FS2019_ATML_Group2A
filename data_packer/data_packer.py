26a030be802b21c224e44afb5dad973797416364
mickael2054 <mickaelgerber@gmail.com>
2019-04-11 16:50:26 +0200

    Implementation of data packer

--- data_packer/data_packer.py
+++ data_packer/data_packer.py
@@ -1,92 +1,44 @@
 import os
-import sys
 import numpy as np
 import PIL
 
-def packFiles(path, filename):   
-    tmp_data = []
-    for file in os.listdir(path):
-        image = PIL.Image.open(path + "/" + file)
-        
-        tmp_data.append(image)
-        
-    np.savez(filename + ".npy", tmp_data)
+fileNameIMGS = "BenignAndMalignant20000DatasetIMG.npy"
+fileNameTAGS = "BenignAndMalignant20000DatasetTAG.npy"
+imagesPath = "BenignAndMalignant20000Dataset"
+
+def packImage(fileNameIMGS, fileNameTAGS, imageName):
     
-print("Packaging")
+    image = PIL.Image.open(imagesPath + "/" + imageName)
 
-packFiles("downloadedFiles/ISIC-images/UDA-1", "testPackage1")
+    if os.path.exists(fileNameIMGS) == 0:
+        image_expand = np.expand_dims(image, 0)
+        np.save(fileNameIMGS, image_expand)
+    
+    else:
+        all_im = np.load(fileNameIMGS)
+        image_expand = np.expand_dims(image, 0)
+        img_cat = np.concatenate((all_im, image_expand))
+        np.save(fileNameIMGS, img_cat)
+            
+        
+    if os.path.exists(fileNameTAGS) == 0:
+        tag_expand = np.expand_dims(imageName[0:1], 0)
+        np.save(fileNameTAGS, tag_expand)
+    
+    else:
+        all_tags = np.load(fileNameTAGS)
+        tag_expand = np.expand_dims(imageName[0:1], 0)
+        tags_cat = np.concatenate((all_tags, tag_expand))
+        np.save(fileNameTAGS, tags_cat)
+        
 
+i = 1
+listOfFiles = os.listdir(imagesPath)
+for file in listOfFiles:
+    
+    print("Packing image " + str(i) + "/" + str(len(listOfFiles)))
+    packImage(fileNameIMGS, fileNameTAGS, file)
+    i = i + 1
+    
 print("Done")
 
-
-
-
-
-
-'''
-
-from __future__ import print_function
-from __future__ import absolute_import
-from distutils.dir_util import copy_tree
-
-import os
-import sys
-import glob
-import json
-import re
-import shutil
-from shutil import copytree, ignore_patterns
-import numpy as np
-
-CONTEXT_LENGTH = 48
-IMAGE_SIZE = 256
-BATCH_SIZE = 64
-EPOCHS = 10
-STEPS_PER_EPOCH = 72000
-
-class Utils:
-    @staticmethod
-    def sparsify(label_vector, output_size):
-        sparse_vector = []
-
-        for label in label_vector:
-            sparse_label = np.zeros(output_size)
-            sparse_label[label] = 1
-
-            sparse_vector.append(sparse_label)
-
-        return np.array(sparse_vector)
-
-    @staticmethod
-    def get_preprocessed_img(img_path, image_size):
-        import cv2
-        img = cv2.imread(img_path)
-        img = cv2.resize(img, (image_size, image_size))
-        img = img.astype('float32')
-        img /= 255
-        return img
-
-    @staticmethod
-    def show(image):
-        import cv2
-        cv2.namedWindow("view", cv2.WINDOW_AUTOSIZE)
-        cv2.imshow("view", image)
-        cv2.waitKey(0)
-        cv2.destroyWindow("view")
-
-print("Converting images to numpy arrays...")
-
-for f in os.listdir(input_path):
-    if f.find(".png") != -1:
-        img = Utils.get_preprocessed_img("{}/{}".format(input_path, f), IMAGE_SIZE)
-        file_name = f[:f.find(".png")]
-
-        np.savez_compressed("{}/{}".format(output_path, file_name), features=img)
-        retrieve = np.load("{}/{}.npz".format(output_path, file_name))["features"]
-
-        assert np.array_equal(img, retrieve)
-
-        shutil.copyfile("{}/{}.gui".format(input_path, file_name), "{}/{}.gui".format(output_path, file_name))
-
-print("Numpy arrays saved in {}".format(output_path))
-'''

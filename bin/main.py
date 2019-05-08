4beece7cf959e5352c4366d6d10985395d2511cd
kennyster92 <kevinceni@gmail.com>
2019-05-08 21:59:07 +0200

    configuration files added

--- bin/main.py
+++ bin/main.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         '--config_file',
         dest="config_file_path",
         type=str,
-        default='./config/config.json',
+        default='./config/config1.json',
         help='Path to the configuration file.'
     )
     parser.add_argument(
